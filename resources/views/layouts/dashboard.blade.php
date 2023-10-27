146693e23d5ce4cbeabf42440f4f753835d888ca
Kiki darmawan <kikiedarmawan@gmail.com>
2023-10-27 14:56:24 +0700

    update foter

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -314,10 +314,11 @@
     <!-- BEGIN: Footer-->
     <footer class="footer footer-static footer-light">
         <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy;
-                2023<a class="ms-25" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Kiki
+                2023<a class="ms-25" href="https://github.com/kikidarmawan" target="_blank">Kiki
                     Darmawan</a><span class="d-none d-sm-inline-block">, All rights
-                    Reserved</span></span><span class="float-md-end d-none d-md-block">Hand-crafted & Made with<i
-                    data-feather="heart"></i></span></p>
+                    Reserved</span></span>
+            <span class="float-md-end d-none d-md-block">Versi 1.0.0</span>
+        </p>
     </footer>
     <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
     <!-- END: Footer-->
